8ece08e045847d6ad4a558babe2042a9651faaa4
MoMansur <mansurshehumsm@gmail.com>
2024-06-15 19:42:59 +0300

    Test

--- Tic-Tac-Toe/main.js
+++ Tic-Tac-Toe/main.js
@@ -1,113 +1,76 @@
-const DOMgrid = document.querySelectorAll('.cell')
-const gameBoxDom = document.querySelector('.gameBox')
-const statusMessage = document.getElementById('statusMessage')
+// DOM elements
+const DOMgrid = document.querySelectorAll('.cell');
+const gameBoxDom = document.querySelector('.gameBox');
+const statusMessage = document.getElementById('statusMessage');
 
-//SCORES
-const playerOneNameDom = document.querySelector('.name1')
-const playerOneScoreDom = document.querySelector('.score1')
+// Score elements
+const playerOneNameDom = document.querySelector('.name1');
+const playerOneScoreDom = document.querySelector('.score1');
+const playerTwoNameDom = document.querySelector('.name2');
+const playerTwoScoreDom = document.querySelector('.score2');
 
-const playerTwoNameDom = document.querySelector('.name2')
-const playerTwoScoreDom = document.querySelector('.score2')
+// Reset button
+const resetScores = document.getElementById('resetScores');
 
-//Reset Buttons
-const resetScores = document.getElementById('resetScores')
-
-
-
-function Player(name, marker,) {
+// Player constructor
+function Player(name, marker) {
   this.name = name;
   this.marker = marker;
   this.score = 0;
   this.winner = function() {
-    console.log(`${this.name} is the winner`)
-    let theScore = this.score++
-    return theScore
+    let theScore = this.score++;
+    return theScore;
   };
-  this.resetBoard = ()=>{
-    console.log(this.winner())
-     return this.score = 0;
-  }
 }
 
-const player1 = new Player('Player One', 'X',);
-const player2 = new Player('Player Two', 'O',);
+// Create players
+const player1 = new Player('Player One', 'X');
+const player2 = new Player('Player Two', 'O');
 
-console.log(player1.score++)
-console.log(player1.score++)
-console.log(player1.score++)
+// Game board module
+function gameBoard() {
+  let board = ['','','',  '','','', '','',''];
 
-
-function scores(){
-  player1
-}
-let scoreX =0;
-let scoreO =0;
-
-
-function gameBoard(){
-  let board = ['','','',  '','','', '','','']; 
-
-function resetBoard(board){
-  board = ['','','',  '','','', '','','']; 
-  gameDisplay(board)
-  return board
-}
-function resetScores(){
-
-}
-
-function gameDisplay(board){
-  DOMgrid.forEach(cell => {
-    const index = parseInt(cell.getAttribute('data-index'));
-    cell.innerHTML = board[index]
-  });
-}
-
-return {board, resetBoard, gameDisplay, resetScores}
-}
-
-const getBoard = () => gameBoard().board
-const getReset = ()=> gameBoard().resetBoard(getBoard())
-
-
-let scoresArray = [player1.winner(), player2.winner()]
-//GAME FUNCTION FUNCION
-function gameFunctions(){
-
-  scoreX = 0
-  function scoreCounter(gameOver){
-    if(gameOver === 'X'){
-      scoreDom('X')
-      resetScores.addEventListener('click', ()=>{
-        player1.resetBoard()
-        console.log('reseted')
-        console.log(scoresArray[0],scoresArray[1])
-        scoreDom('X')
-
-      })
-      console.log(scoresArray[0]++,scoresArray[1]++)
-    }else if(gameOver === 'O'){
-      scoreDom('O') 
-      console.log(scoresArray[0],scoresArray[1])
-
-    }
+  // Reset the board
+  function resetBoard(board) {
+    board = ['','','',  '','','', '','',''];
+    gameDisplay(board);
+    return board;
   }
 
-  function animateCells(indexes, color1, color2, duration,) {
+  // Display the board
+  function gameDisplay(board) {
+    DOMgrid.forEach(cell => {
+      const index = parseInt(cell.getAttribute('data-index'));
+      cell.innerHTML = board[index];
+    });
+  }
+
+  return { board, resetBoard, gameDisplay };
+}
+
+const getBoard = () => gameBoard().board;
+const getReset = () => gameBoard().resetBoard(getBoard());
+
+let scoresArray = [player1.score++, player2.score++];
+
+// Game functions module
+function gameFunctions() {
+
+ 
+
+  // Animate winning cells
+  function animateCells(indexes, color1, color2, duration) {
     indexes.forEach(index => {
       const cell = document.querySelector(`.cell[data-index="${index}"]`);
       if (cell) {
         let isColor1 = true;
-        let font = true
+        let font = true;
         const intervalId = setInterval(() => {
           cell.style.backgroundColor = isColor1 ? color1 : color2;
           cell.style.fontSize = font ? '5rem' : '3rem';
-        
-          statusMessage.innerHTML =` ${cell.textContent } is the winner`;
-
           isColor1 = !isColor1;
-          font = !font
-
+          font = !font;
         }, 500);
 
         setTimeout(() => {
@@ -119,6 +82,7 @@ function gameFunctions(){
     });
   }
 
+  // Check for winning combinations
   const checkForWins = (board) => {
     const winningCombos = [
       [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -129,136 +93,148 @@ function gameFunctions(){
     for (const positions of winningCombos) {
       const [a, b, c] = positions;
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        const theWinIndex = [a, b, c]; 
-        animateCells(theWinIndex, 'rgb(39, 76, 74)', 'lightseagreen', 2000);
-        return board[a]
+        const theWinIndex = [a, b, c];
+       
+        // animateCells(theWinIndex, 'rgb(39, 76, 74)', 'lightseagreen', 2000);
+  
+        return board[a];
       }
     }
-      if (board.every(cell => cell !== "")) {
-      console.log('tIe') 
+
+    if (board.every(cell => cell !== "")) {
+      console.log('tie');
       statusMessage.innerText = 'A TIE';
-      return true
+      return true;
     }
-      return false
+    return false;
   };
 
-  function scoreDom(x){
-    if(x === 'X'){
-      playerOneNameDom.innerHTML = `${player1.name} (${player1.marker})`
+ function scoreHandler(win){
+
+  scoreDom(win)
+ }
+
+  // Update the DOM with the scores
+  function scoreDom(x) {
+    if(x === 'X') {
+      playerOneNameDom.innerHTML = `${player1.name} (${player1.marker})`;
       playerOneScoreDom.innerHTML = scoresArray[0]++;
+
+      statusMessage.innerHTML = `${player1.marker} is the winner`;
+      setTimeout(() => {
+      statusMessage.innerHTML = ''; 
+    }, 2000);
+    } else if('O') {
+      playerTwoNameDom.innerHTML = `${player2.name} (${player2.marker})`;
+      playerTwoScoreDom.innerHTML = scoresArray[1]++;
+      
+      statusMessage.innerHTML = `${player2.marker} is the winner`;
+     
+      setTimeout(() => {
+      statusMessage.innerHTML = ''; 
+    }, 2000);
+      
     }else{
-      playerTwoNameDom.innerHTML = `${player2.name} (${player2.marker})`
-      playerTwoScoreDom.innerHTML =  scoresArray[1]++;
+      statusMessage.innerHTML = `A Tie`;
+      setTimeout(() => {
+      statusMessage.innerHTML = ''; 
+    }, 2000);
     }
   }
 
-return {checkForWins, scoreCounter}
+
+  return { checkForWins, scoreHandler,  scoreDom, animateCells};
 }
 
- statusMessage.innerText = 'Play'
-//GAME PLAY FUNCTION
-function gamePlay(){
+// Game play module
+function gamePlay() {
 
   let activePlayer = player1;
 
+  // Switch player turn
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === player1 ? player2 : player1;
-    statusMessage.innerText = 'Start'
-    statusMessage.innerText = `${activePlayer.marker} is your Turn`
+    statusMessage.innerText = 'Start';
+    statusMessage.innerText = `${activePlayer.marker} is your Turn`;
+  }
+
+  // Get active player
+  const getActivePlayer = () => activePlayer;
+
+  
+  
+  // Insert player marker
+  function playerInsertMarker(board) {
+    gameBoxDom.addEventListener('click', (e) => {
+      const clickedGrid = e.target;
+      const index = parseInt(clickedGrid.getAttribute('data-index'));
+
+      if(board[index] !== "") {
+        board[index] = board[index];
+      } else {
+        board[index] = getActivePlayer().marker;
+        switchPlayerTurn();
+      }
+
+      const gameDisplay = () => gameBoard().gameDisplay(board);
+      gameDisplay(board);
+      gameOverFunc(board)
+      let gameOver = gameFunctions().checkForWins(board);
+  
+
+    });
     
   }
 
-  const getActivePlayer = () => activePlayer
 
-  function gameOverFunc(gameOver, board) {
-    if (gameOver) {
-  
-      // Reset board after 2 seconds
-      setTimeout(() => {
-        resetBoard(board);
-      }, 2000);
-  
-      // Reset scores immediately
-      resetScore();
+  // // Start a round
+  function playRound(board) {
+    
+    DOMgrid.forEach(cell=>{
+      cell.innerHTML = ""
+      getReset()
+    })
+  playerInsertMarker(board)
+
+ 
+ 
+  }
+
+  function playNewRound(board, gameOverFunc){
+    // playRound(board)
+
+    console.log(gameOverFunc)
+    if(gameOverFunc === 'X'){
+      console.log('Its over')
+      DOMgrid.forEach(cell=>{
+        cell.innerHTML = ""
+        playRound(getBoard())
+      })
+     
+    }else{
+      console.log('Its not')
     }
   }
   
-  function resetBoard(board) {
-    // Clear the board array
-    board.forEach((_, index) => {
-      board[index] = '';
-    });
-  
-    // Update the UI to reflect the cleared board
-    getReset();
-  }
+ 
 
 
 
-  function playerInsertMarker(board){
-    gameBoxDom.addEventListener('click', (e)=>{
-      const clickedGrid = e.target
-      const index = parseInt(clickedGrid.getAttribute('data-index'));
-
-      if(board[index] !== ""){
-        board[index]= board[index]
-      }else{
-        board[index] = getActivePlayer().marker
-        switchPlayerTurn()
-      }     
-
-   
-
-      const gameDisplay = () => gameBoard().gameDisplay(board)
-      gameDisplay(board)
-
-      
-      let gameOver = gameFunctions().checkForWins(board)
-      gameFunctions().scoreCounter(gameOver);
-
-      
-        
-      gameOverFunc(gameOver, board)
-
-      
-      
-    }) 
-
-  }
-
-  
-
-  function playRound(board){
-  playerInsertMarker(board)
-
-
-  }
-
-
-  function resetScore(){
-    scoreX =0;
-    scoreO =0;
-
-  }
-  resetScores.addEventListener('click', ()=>{
-  
-    let scoresArray = [0, 0]
-
-   
-   })
-
-
-  return{playRound,playerInsertMarker, getActivePlayer, switchPlayerTurn, resetScore}
+  return { playRound, playNewRound, playerInsertMarker, getActivePlayer, switchPlayerTurn };
 }
 
+// Initialize the game
+const game = gamePlay();
+game.playRound(getBoard());
+
+// game.playNewRound(getBoard())
+
+const playAgain = document.getElementById('playAgain').addEventListener('click', ()=>{
+  game.playRound(getBoard());
+
+})
 
 
-const game = gamePlay()
-
-
-game.playRound(getBoard())
-
-
-
-
-
+resetScores.addEventListener('click', () => {
+  scoresArray = [0, 0];
+});
